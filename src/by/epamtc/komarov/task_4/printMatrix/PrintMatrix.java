f1b322f4702f474c56c885a2cfe6b22a2941e083
AntonKomarov <anton.komarov116@gmail.com>
2020-06-27 15:20:46 +0300

    Homework array

---
+++ src/by/epamtc/komarov/task_4/printMatrix/PrintMatrix.java
@@ -0,0 +1,14 @@
+package by.epamtc.komarov.task_4.printMatrix;
+
+public class PrintMatrix {
+
+    public static void print(int[][] matrix){
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
