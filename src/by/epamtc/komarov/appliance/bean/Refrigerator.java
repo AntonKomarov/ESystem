23a5471dbbdf31106f354f508f07037d7310ad92
AntonKomarov <anton.komarov116@gmail.com>
2020-07-18 01:32:16 +0300

    work with layered architecture

--- src/by/epamtc/komarov/appliance/bean/Refrigerator.java
+++ src/by/epamtc/komarov/appliance/bean/Refrigerator.java
@@ -16,47 +16,66 @@ public class Refrigerator implements Appliance{
         return power_consumption;
     }
 
-    public void setPower_consumption(int power_consumption) {
-        this.power_consumption = power_consumption;
-    }
-
     public int getWeight() {
         return weight;
-    }
-
-    public void setWeight(int weight) {
-        this.weight = weight;
     }
 
     public int getFreezer_capacity() {
         return freezer_capacity;
     }
 
-    public void setFreezer_capacity(int freezer_capacity) {
-        this.freezer_capacity = freezer_capacity;
-    }
-
     public int getOverall_capacity() {
         return overall_capacity;
-    }
-
-    public void setOverall_capacity(int overall_capacity) {
-        this.overall_capacity = overall_capacity;
     }
 
     public int getHeight() {
         return height;
     }
 
-    public void setHeight(int height) {
-        this.height = height;
-    }
-
     public int getWidth() {
         return width;
     }
 
-    public void setWidth(int width) {
-        this.width = width;
+
+    public static class RefrigeratorBuilder {
+        Refrigerator refrigerator;
+
+        public RefrigeratorBuilder(){
+            refrigerator = new Refrigerator();
+        }
+
+        public RefrigeratorBuilder setPower_consumption(int power_consumption) {
+            refrigerator.power_consumption = power_consumption;
+            return this;
+        }
+
+        public RefrigeratorBuilder setWeight(int weight) {
+            refrigerator.weight = weight;
+            return this;
+        }
+
+        public RefrigeratorBuilder setFreezer_capacity(int freezer_capacity) {
+            refrigerator.freezer_capacity = freezer_capacity;
+            return this;
+        }
+
+        public RefrigeratorBuilder setOverall_capacity(int overall_capacity) {
+            refrigerator.overall_capacity = overall_capacity;
+            return this;
+        }
+
+        public RefrigeratorBuilder setHeight(int height) {
+            refrigerator.height = height;
+            return this;
+        }
+
+        public RefrigeratorBuilder setWidth(int width) {
+            refrigerator.weight = width;
+            return this;
+        }
+
+        public Refrigerator refrigeratorBuild(){
+            return refrigerator;
+        }
     }
 }
