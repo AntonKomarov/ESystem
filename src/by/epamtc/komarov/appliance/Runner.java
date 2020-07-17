bef5adfc04cb8a944a95ad8ce83eedc75dba76fc
AntonKomarov <anton.komarov116@gmail.com>
2020-07-17 21:47:17 +0300

    work with layered architecture

--- src/by/epamtc/komarov/appliance/Runner.java
+++ src/by/epamtc/komarov/appliance/Runner.java
@@ -1,18 +1,17 @@
-package by.epamtc.komarov.file;
+package by.epamtc.komarov.appliance;
 
-import by.epamtc.komarov.file.helper.Print;
-import by.epamtc.komarov.file.technic.*;
+import by.epamtc.komarov.appliance.dao.FindApplianceImpl;
 
 public class Runner {
 
      public static void main(String[] args) {
 
-        Print.printGood("Oven", "height 40");
+        FindApplianceImpl.printGood("Oven", "height 40");
 //        Print.printGood(new Laptop(), "memory rom 8000");
 //        Print.printGood(new Speakers(), "FREQUENCY RANGE = 2-4");
 //        Print.printGood(new TabletPC(), "Red");
 //        Print.printGood(new Refrigerator(), "WEIGHT 30");
-        Print.printGood("VacuumCleaner", "POWER_CONSUMPTION=100");
+        FindApplianceImpl.printGood("VacuumCleaner", "POWER_CONSUMPTION=100");
 
     }
 }
